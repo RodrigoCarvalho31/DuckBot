26e81b1d109c933d18ae026f305d2552eebf51e9
RodrigoCarvalho31 <rodrigo.oliveirac31@gmail.com>
2019-06-04 19:20:14 -0300

    Sla

--- src/main/java/net/rodrigocarvalho/duckbot/Main.java
+++ src/main/java/net/rodrigocarvalho/duckbot/Main.java
@@ -4,9 +4,9 @@ public class Main {
 
     public static void main(String[] args) {
         DuckBot bot = DuckBot.getInstance();
-        bot.setToken("NTcxNDUwMzYzMjM2OTc0NzYw.XMN7Iw.c8McLkBrRj_VuUk8KNIRLl-8Mh8");
-        bot.setUsername("rodrigocarvalho52");
-        bot.setPassword("Duckarai0");
+        bot.setToken("token");
+        bot.setUsername("user");
+        bot.setPassword("password");
         bot.start();
     }
 }
